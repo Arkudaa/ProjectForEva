e02ff10a33bbc939e2ace3d09ccf415f0cc469bb
Arkudaa <71982604+Arkudaa@users.noreply.github.com>
2022-04-11 14:29:38 +0300

    vesr1.0

---
+++ EvaProject1/DigitalYouth-main/New Project/Assets/My Game/My Scripts/NPC_1.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPC_1 : NPC {
+	 
+	// Use this for initialization
+	public override void OnSetUpDialogue()
+	{
+		Speech.AddDialogue("0", "Hello, you must be one of the humans in fantasy world", "1");
+		Speech.AddDialogue("1", "Our worlds are in great danger, We need your help!", "2");
+		Speech.AddDialogue("2", "Please help us save our worlds - find tower", "3");
+		Speech.AddDialogue("3", "Find powerup to jump higher before going to the next island");
+
+		Speech.AddDialogue("4", "I see you have already gained your first ability. When you're you can head to the corrupted fantasy island - find tower.");
+	}
+	
+
+	// Triggered when the player comes in range of the NPC
+	public override void OnTriggerNPC( Collider other )
+	{ if(Abilities.doubleJumpEnabled)
+        {
+			Speech.SetDialogue("4");
+		}
+	else
+        {
+			Speech.SetDialogue("0");
+		}
+		
+	}
+}
---
+++ EvaProject1/DigitalYouth-main/New Project/Assets/My Game/My Scripts/PlayerScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	public int coin;
+
+	// Start is called on the frame when a script is enabled just before any of the Update methods is called the first time.
+	protected void Start() {
+		
+	}
+
+	// OnTriggerEnter is called when the Collider "collided" enters the trigger.
+	protected void OnTriggerEnter(Collider collided) {
+
+		// Check for collision with coins
+		if (collided.gameObject.tag == "Coin") {  
+			
+			Destroy(collided.gameObject);
+			coin++;
+			HUD.Message("coin is collected!");
+				HUD.UpdateCoinCount(coin);
+			
+		}
+		if(collided.gameObject.name == "powerup1"){
+			Destroy(collided.gameObject);
+		HUD.Message("You can jump higher");
+		Abilities.doubleJumpEnabled = true;
+		 
+		}
+		if(collided.gameObject.name == "Powerup2")
+        {
+			Destroy(collided.gameObject);
+			Abilities.spinAttackEnabled = true;
+        }
+	}
+}
